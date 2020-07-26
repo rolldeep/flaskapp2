5880e999eb9b7c7040ad09780edf2bf6bbc04976
analytics_gitlab <bi@selectel.ru>
2020-07-26 13:33:45 +0300

    succesfully filled in db

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+from flask_migrate import Migrate
 import json
-from teachers import Teacher, Teachers
-from booking import Booking, TeacherRequest
+from booking import TeacherRequest
 from forms import BookingForm, RequestForm
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'noneofthemwouldgeas19203332'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-app.secret_key = 'noneofthemwouldgeas19203332'
-t = Teachers()
+
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    about = db.Column(db.String(4000))
+    rating = db.Column(db.Integer)
+    picture = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    goals = db.Column(db.Text)
+    free = db.Column(JSON)
+
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    weekday = db.Column(db.DateTime(), nullable=False)
+    time = db.Column(db.Time(), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(255), nullable=False)
+
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+    teacher = db.relationship('Teacher')
+
+
+class TeacherRequest(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    goal = db.Column(db.String(30), nullable=False)
+    availability = db.Column(db.String(30), nullable=False)
+    clientName = db.Column(db.String(30), nullable=False)
+    clientPhone = db.Column(db.String(30), nullable=False)
 
 
 @app.route('/')
@@ -15,12 +56,12 @@ def main():
     return render_template('index.html', teachers_list=t.get_random())
 
 
-@app.route('/goals/<goal>/')
-def get_goal(goal):
-    goal_title = t.goals[goal]
-    teachers_list = [
-        teacher for teacher in t.teachers if goal in teacher['goals']]
-    return render_template('goal.html', teachers_list=teachers_list, goal_title=goal_title)
+# @app.route('/goals/<goal>/')
+# def get_goal(goal):
+#     goal_title = t.goals[goal]
+#     teachers_list = [
+#         teacher for teacher in t.teachers if goal in teacher['goals']]
+#     return render_template('goal.html', teachers_list=teachers_list, goal_title=goal_title)
 
 
 @app.route('/profiles/<id>/')
@@ -103,6 +144,22 @@ def save_booking():
 # Убрал route, для воспроизведения корректной валидации формы
 # @app.route('/booking_done/', methods=['POST'])
 
+def fill_db():
+    with open('base.json') as f:
+        teachers = json.load(f)['teachers']
+    for t in teachers:
+        t_db = Teacher(
+            name=t['name'],
+            about=t['about'],
+            rating=t['rating'],
+            picture=t['picture'],
+            price=t['price'],
+            goals=t['goals'],
+            free=t['free']
+        )
+        db.session.add(t_db)
+        db.session.commit()
 
 if __name__ == "__main__":
+    fill_db()
     app.run(debug=True)
